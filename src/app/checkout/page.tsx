9ab1c9a7b272e034925bdb8aec9df48334e9b06c
mithil0407 <mithil0407@gmail.com>
2025-10-24 15:32:42 +0530

    1399

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -76,7 +76,7 @@ export default function CheckoutPage() {
   
   // Product pricing
   const originalPrice = 5999;
-  const discountedPrice = 1299;
+  const discountedPrice = 1399;
   const savings = originalPrice - discountedPrice;
   
   // Add-ons
